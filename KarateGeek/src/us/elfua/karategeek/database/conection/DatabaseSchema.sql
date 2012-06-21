bd3090e2b6b21114c01a3b2f10e86e24da992dea
michael theodorides <mc.theodorides@gmail.com>
2012-06-21 11:43:25 +0300

    database

--- KarateGeek/src/us/elfua/karategeek/database/conection/DatabaseSchema.sql
+++ KarateGeek/src/us/elfua/karategeek/database/conection/DatabaseSchema.sql
@@ -1,16 +1,38 @@
 create table Athelets 
 (
-primary key Athlet_ID  nvarchar(50),
-First_Name nvarchar(50),
+primary key Athlet_ID  int,
 Last_Name nvarchar(50),
-Age int
-
+First_Name nvarchar(50),
+Sex nvarchar(15),
+Date_of_Birth date,
+Belt_Level nvarchar(15),
+Phone_Number nvarchar(15),
+Email nvarchar(50),
+Team_ID int references teams(Team_ID)
 
 );
 
 
+create Teams
+(
+Team_ID int,
+Team_Name nvarchar(50),
+Phone_Number nvarchar(15)
+
+);
+
+
+
+
 create table Judges
 (
+primary key Judge_ID  int,
+Last_Name nvarchar(50),
+First_Name nvarchar(50),
+Sex nvarchar(15),
+Phone_Number nvarchar(15),
+Email nvarchar(50),
+Address  nvarchar(50)
 
 );
 
