6dc5820a40d8682c47b7e826b74b551e11d64de1
michael theodorides <mc.theodorides@gmail.com>
2012-06-21 11:27:51 +0300

    database schema

---
+++ KarateGeek/src/us/elfua/karategeek/database/conection/DatabaseSchema.sql
@@ -0,0 +1,35 @@
+create table Athelets 
+(
+primary key Athlet_ID  nvarchar(50),
+First_Name nvarchar(50),
+Last_Name nvarchar(50),
+Age int
+);
+
+
+create table Judges
+(
+
+
+
+
+);
+
+
+
+create table Events
+(
+
+
+);
+
+
+
+
+create table games
+(
+
+
+);
+
+
