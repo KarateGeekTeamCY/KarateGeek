1eff7f70f2ec4a7226e71d02cacffd43c59ce6ae
michael theodorides <mc.theodorides@gmail.com>
2012-09-04 17:24:31 +0300

    sql fix

--- KarateGeek/src/us/elfua/karategeek/database/conection/DatabaseSchema.sql
+++ KarateGeek/src/us/elfua/karategeek/database/conection/DatabaseSchema.sql
@@ -19,113 +19,113 @@
 --     
 -- 
 
-drop table `karategeekdb`.`atheletes`;
-drop table `karategeekdb`.`teams`;
-drop table `karategeekdb`.`judges`;
-drop table `karategeekdb`.`events`;
-drop table `karategeekdb`.`event_participations`;
-drop table `karategeekdb`.`event_winners`;
-drop table `karategeekdb`.`game_participations`;
-drop table `karategeekdb`.`athlete_scores`;
--- drop table `karategeekdb`.``;
+drop table karategeekdb.atheletes;
+drop table karategeekdb.teams;
+drop table karategeekdb.judges;
+drop table karategeekdb.events;
+drop table karategeekdb.event_participations;
+drop table karategeekdb.event_winners;
+drop table karategeekdb.game_participations;
+drop table karategeekdb.athlete_scores;
+-- drop table karategeekdb.;
 
 
-create table `karategeekdb`.`atheletes` (
-`athlete_id`  int,
-`last_name` nvarchar(50),
-`first_name` nvarchar(50),
-`sex` nvarchar(15),
-`date_of_birth` date,
-`belt_level` nvarchar(15),
-`phone_number` nvarchar(15),
-`email` nvarchar(50),
-`team_id` int referenses `karategeekdb`.`teams`( `team_id` ),
-primary key ( `athlete_id` )
+create table karategeekdb.atheletes (
+athlete_id  int,
+last_name nvarchar(50),
+first_name nvarchar(50),
+sex nvarchar(15),
+date_of_birth date,
+belt_level nvarchar(15),
+phone_number nvarchar(15),
+email nvarchar(50),
+team_id int referenses karategeekdb.teams( team_id ),
+primary key ( athlete_id )
 );
 
 
-create table `karategeekdb`.`teams`(
+create table karategeekdb.teams(
 team_id int,
 team_name nvarchar(50),
 Phone_Number nvarchar(15),
-primary key ( `team_id` )
+primary key ( team_id )
 );
 
 
 
 
-create table `karategeekdb`.`judges` (
-`judge_id`  int,
-`last_name` nvarchar(50),
-`first_name` nvarchar(50),
-`sex` nvarchar(15),
-`phone_number` nvarchar(15),
-`email` nvarchar(50),
-`address`  nvarchar(50),
-primary key ( `judge_id` )
+create table karategeekdb.judges (
+judge_id  int,
+last_name nvarchar(50),
+first_name nvarchar(50),
+sex nvarchar(15),
+phone_number nvarchar(15),
+email nvarchar(50),
+address  nvarchar(50),
+primary key ( judge_id )
 );
 
 
 
 
-create table `karategeekdb`.`events` (
-`event_id` int,
-`event_name` nvarchar(50),
-`event_date` date,
-`event_location` nvarchar(50),
-`event_catecory` nvarchar(50),
-`event_level` nvarchar(50),
-`event_type` nvarchar(50),
-`event_scoring_System` nvarchar(50),
-primary key ( `event_id` )
+create table karategeekdb.events (
+event_id int,
+event_name nvarchar(50),
+event_date date,
+event_location nvarchar(50),
+event_catecory nvarchar(50),
+event_level nvarchar(50),
+event_type nvarchar(50),
+event_scoring_System nvarchar(50),
+primary key ( event_id )
 );
 
 
 
 
-create table `karategeekdb`.`Event_Participations` (
-`athlet_id` int references `karategeekdb`.`athletes`( `athlete_id` ) ,
-`event_id` int references `karategeekdb`.`events`( `event_id` ) ,
-`level_at_time` nvarchar(20) ,
-`team` int,
-primary key ( `athlet_id` , `event_id` )
+create table karategeekdb.Event_Participations (
+athlet_id int references karategeekdb.athletes( athlete_id ) ,
+event_id int references karategeekdb.events( event_id ) ,
+level_at_time nvarchar(20) ,
+team int,
+primary key ( athlet_id , event_id )
 );
 
 
-create table `karategeekdb`.`event_winners`(
-`athlet_id` int references `karategeekdb`.`athletes`( `athlete_id` ) ,
-`event_id` int `karategeekdb`.`events`( `event_id` ) ,
-`possition` int ,
-primary key(`athlet_id`, `event_id` )
-);
-
-
-
-create table `karategeekdb`.`games` (
-`game_id` int ,
-`event_id` int `karategeekdb`.`events`( `event_id` ) ,
-`game_phase` int ,
-`game_position` int ,
-primary key (`game_id`)
+create table karategeekdb.event_winners(
+athlet_id int references karategeekdb.athletes( athlete_id ) ,
+event_id int karategeekdb.events( event_id ) ,
+possition int ,
+primary key(athlet_id, event_id )
 );
 
 
 
-create table `karategeekdb`.`game_participations`(
-`game_id` int references `karategeekdb`.`games`( `game_id` ) ,
-`athlte_id` int references `karategeekdb`.`athletes`( `athlete_id` ) ,
-primary key(`game_id` , `athlete_id` )
+create table karategeekdb.games (
+game_id int ,
+event_id int karategeekdb.events( event_id ) ,
+game_phase int ,
+game_position int ,
+primary key (game_id)
 );
 
 
 
-create table `karategeekdb`.`athlete_scores` (
-`game_id` int ,
-`athlete_id` int references `karategeekdb`.`athletes` ( `athlete_id` ),
-`judge_id` int references `karategeekdb`.`judges` ( `judges_id` ) ,
-`score` int,
-`score_type` nvarchar(30)
-primary key ( `game_id` , `athlete_id` )
+create table karategeekdb.game_participations(
+game_id int references karategeekdb.games( game_id ) ,
+athlte_id int references karategeekdb.athletes( athlete_id ) ,
+primary key(game_id , athlete_id )
+);
+
+
+
+create table karategeekdb.athlete_scores (
+game_id int ,
+athlete_id int references karategeekdb.athletes ( athlete_id ),
+judge_id int references karategeekdb.judges ( judges_id ) ,
+score int,
+score_type nvarchar(30)
+primary key ( game_id , athlete_id )
 );
 
 
