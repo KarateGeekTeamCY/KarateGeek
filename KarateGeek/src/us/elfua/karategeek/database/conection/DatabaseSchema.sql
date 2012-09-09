22fd240d81e600a3eff8a30850532be1cfdb03e6
NicholasKa <nicholaskar+git@googlemail.com>
2012-09-09 21:22:02 +0300

    Fixed the database table creation script to work with pgAdminIII/PostgreSQL. Tested with PostgreSQL version 8.4 (should be okay with 9.2). DB schema is still crap.

--- KarateGeek/src/us/elfua/karategeek/database/conection/DatabaseSchema.sql
+++ KarateGeek/src/us/elfua/karategeek/database/conection/DatabaseSchema.sql
@@ -2,7 +2,7 @@
 --                        karate geek
 -- 
 --         
---     Copyright (C) 2012  michael theodorides <mc.theodorides@gmail.com>
+--     Copyright (C) 2012  m̶i̶c̶h̶a̶e̶l̶ ̶t̶h̶e̶o̶d̶o̶r̶i̶d̶e̶s̶ ̶<̶m̶c̶.̶t̶h̶e̶o̶d̶o̶r̶i̶d̶e̶s̶@̶g̶m̶a̶i̶l̶.̶c̶o̶m̶>̶ :-P
 -- 
 --     This program is free software: you can redistribute it and/or modify
 --     it under the terms of the GNU General Public License as published by
@@ -17,120 +17,112 @@
 --     You should have received a copy of the GNU General Public License
 --     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 --     
--- 
-
-drop table karategeekdb.atheletes;
-drop table karategeekdb.teams;
-drop table karategeekdb.judges;
-drop table karategeekdb.events;
-drop table karategeekdb.event_participations;
-drop table karategeekdb.event_winners;
-drop table karategeekdb.game_participations;
-drop table karategeekdb.athlete_scores;
--- drop table karategeekdb.;
-
-create schema karategeekdb;
+--
 
 
-create table karategeekdb.atheletes (
-athlete_id  int,
-last_name varchar(50),
-first_name varchar(50),
-sex varchar(15),
-date_of_birth date,
-belt_level varchar(15),
-phone_number varchar(15),
-email varchar(50),
-team_id int referenses karategeekdb.teams( team_id ),
-primary key ( athlete_id )
+-- begin transaction:
+BEGIN;
+
+
+drop schema IF EXISTS schema_v1 CASCADE; -- WARNING: This also deletes all tables!
+
+
+create schema schema_v1;
+
+
+-- table creation:
+create table schema_v1.teams(
+	team_id int,
+	team_name varchar(50),
+	Phone_Number varchar(15),
+	primary key ( team_id )
 );
 
 
-create table karategeekdb.teams(
-team_id int,
-team_name varchar(50),
-Phone_Number varchar(15),
-primary key ( team_id )
+create table schema_v1.athletes (
+	athlete_id  int,
+	last_name varchar(50),
+	first_name varchar(50),
+	sex varchar(15),
+	date_of_birth date,
+	belt_level varchar(15),
+	phone_number varchar(15),
+	email varchar(50),
+	team_id int references schema_v1.teams( team_id ),
+	primary key ( athlete_id )
 );
 
 
-
-
-create table karategeekdb.judges (
-judge_id  int,
-last_name varchar(50),
-first_name varchar(50),
-sex varchar(15),
-phone_number varchar(15),
-email varchar(50),
-address  varchar(50),
-primary key ( judge_id )
+create table schema_v1.judges (
+	judge_id  int,
+	last_name varchar(50),
+	first_name varchar(50),
+	sex varchar(15),
+	phone_number varchar(15),
+	email varchar(50),
+	address  varchar(50),
+	primary key ( judge_id )
 );
 
 
-
-
-create table karategeekdb.events (
-event_id int,
-event_name varchar(50),
-event_date date,
-event_location varchar(50),
-event_catecory varchar(50),
-event_level varchar(50),
-event_type varchar(50),
-event_scoring_System varchar(50),
-primary key ( event_id )
+create table schema_v1.events (
+	event_id int,
+	event_name varchar(50),
+	event_date date,
+	event_location varchar(50),
+	event_catecory varchar(50),
+	event_level varchar(50),
+	event_type varchar(50),
+	event_scoring_System varchar(50),
+	primary key ( event_id )
 );
 
 
-
-
-create table karategeekdb.Event_Participations (
-athlet_id int references karategeekdb.athletes( athlete_id ) ,
-event_id int references karategeekdb.events( event_id ) ,
-level_at_time varchar(20) ,
-team int,
-primary key ( athlet_id , event_id )
+create table schema_v1.Event_Participations (
+	athlete_id int references schema_v1.athletes( athlete_id ) ,
+	event_id int references schema_v1.events( event_id ) ,
+	level_at_time varchar(20) ,
+	team int,
+	primary key ( athlete_id , event_id )
 );
 
 
-create table karategeekdb.event_winners(
-athlet_id int references karategeekdb.athletes( athlete_id ) ,
-event_id int references karategeekdb.events( event_id ) ,
-possition int ,
-primary key(athlet_id, event_id )
+create table schema_v1.event_winners(
+	athlete_id int references schema_v1.athletes( athlete_id ) ,
+	event_id int references schema_v1.events( event_id ) ,
+	possition int ,
+	primary key(athlete_id, event_id )
 );
 
 
-
-create table karategeekdb.games (
-game_id int ,
-event_id int references karategeekdb.events( event_id ) ,
-game_phase int ,
-game_position int ,
-primary key (game_id)
+create table schema_v1.games (
+	game_id int ,
+	event_id int references schema_v1.events( event_id ) ,
+	game_phase int ,
+	game_position int ,
+	primary key (game_id)
 );
 
 
-
-create table karategeekdb.game_participations(
-game_id int references karategeekdb.games( game_id ) ,
-athlte_id int references karategeekdb.athletes( athlete_id ) ,
-primary key (game_id , athlete_id )
+create table schema_v1.game_participations(
+	game_id int references schema_v1.games( game_id ) ,
+	athlete_id int references schema_v1.athletes( athlete_id ) ,
+	primary key (game_id , athlete_id )
 );
 
 
-
-create table karategeekdb.athlete_scores (
-game_id int ,
-athlete_id int references karategeekdb.athletes ( athlete_id ),
-judge_id int references karategeekdb.judges ( judges_id ) ,
-score int,
-score_type varchar(30)
-primary key ( game_id , athlete_id )
+create table schema_v1.athlete_scores (
+	game_id int ,
+	athlete_id int references schema_v1.athletes ( athlete_id ),
+	judge_id int references schema_v1.judges ( judge_id ) ,
+	score int,
+	score_type varchar(30),
+	primary key ( game_id , athlete_id )
 );
 
 
+-- rollback transaction (useful for checking syntax):
+--ROLLBACK;
 
-
-
+-- commit transaction:
+COMMIT;
