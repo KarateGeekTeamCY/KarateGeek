b193f9b7f363c4b0215f7db307b15c4d9ee118fa
michael theodorides <mc.theodorides@gmail.com>
2012-09-11 10:20:32 +0300

    package refactored

--- KarateGeek/src/us/elfua/karategeek/dataConnection/DataBaseConection.java
+++ KarateGeek/src/us/elfua/karategeek/dataConnection/DataBaseConection.java
@@ -17,7 +17,7 @@
  along with this program.  If not, see <http://www.gnu.org/licenses/>.
     
  */
-package us.elfua.karategeek.database.conection;
+package us.elfua.karategeek.dataConnection;
 
 /**
  *
